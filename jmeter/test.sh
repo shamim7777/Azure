f9e1ee81693b358e5f5d1689bfbed5aa9bb993f4
Shamim Ahmed <shamim@Rizwanas-MacBook-Pro.local>
2020-08-15 18:29:03 +1200

    3nd commit

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -7,3 +7,26 @@ host=$3
 echo "Root path: $rootPath"
 echo "Test file: $testFile"
 echo "Host: $host"
+
+T_DIR=${PWD}
+
+# Reporting dir: start fresh
+R_DIR=$T_DIR/report
+rm -rf ${R_DIR} > /dev/null 2>&1
+mkdir -p $R_DIR
+
+rm -f ${T_DIR}/test/test-plan.jtl ${T_DIR}/test/jmeter.log  > /dev/null 2>&1
+
+bash run.sh $rootPath -Dlog_level.jmeter=DEBUG \
+	-Jhost=$host \
+	-n -t $T_DIR/test/$testFile -l $T_DIR/test/test-plan.jtl -j $T_DIR/test/jmeter.log \
+	-e -o $R_DIR
+
+echo "==== jmeter.log ===="
+cat $T_DIR/test/jmeter.log
+
+echo "==== Raw Test Report ===="
+cat $T_DIR/test/test-plan.jtl
+
+echo "==== HTML Test Report ===="
+echo "See HTML test report in $R_DIR/index.html"
