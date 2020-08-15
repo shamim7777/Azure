7664844b4d0019ce4f8e562cad2c941bcac98fb2
Shamim Ahmed <shamim@Rizwanas-MacBook-Pro.local>
2020-08-16 00:54:42 +1200

    Load test Ultimate thread group

--- jmeter/run.sh
+++ jmeter/run.sh
@@ -8,7 +8,7 @@ IMAGE="egaillardon/jmeter-plugins"
 # Finally run
 sudo docker stop ${NAME} > /dev/null 2>&1
 sudo docker rm ${NAME} > /dev/null 2>&1
-sudo docker --env JMETER_GROUP_ID=/usr/bin/id -g--env JMETER_USER_ID=/usr/bin/id run --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+sudo docker run --name ${NAME} --env JMETER_GROUP_ID="/usr/bin/id -g" --env JMETER_USER_ID="/usr/bin/id -i" -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
 
 
  
